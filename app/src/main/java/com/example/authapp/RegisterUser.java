d76fd88d619c4f5684b59bb067ab89c16f767646
Samson Akanbi <samson.akanbi@outlook.com>
2021-01-06 14:43:59 +0100

    Installed Firebase to the application.

--- app/src/main/java/com/example/authapp/RegisterUser.java
+++ app/src/main/java/com/example/authapp/RegisterUser.java
@@ -4,10 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class RegisterUser extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        mAuth = FirebaseAuth.getInstance();
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_user);
     }
